970b1eab81f3e11b9d5ef2a29df96683c03a705f
Cris_Cui <cxj2856801855@gmail.com>
2023-11-09 10:38:15 +0800

    goto version2

--- store.go
+++ store.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/gob"
+	"io"
 	"log"
 	"os"
 	"sync"
@@ -28,6 +29,10 @@ func NewURLStore(filename string) *URLStore {
 		log.Fatal("URLStore: ", err)
 	}
 	s.file = f
+	// 从磁盘中加载数据到map中
+	if err := s.load(); err != nil {
+		log.Println("Error loading data in URLStore: ", err)
+	}
 	return s
 }
 
@@ -61,17 +66,40 @@ func (s *URLStore) Put(url string) string {
 	for { // for死循环一直尝试keygen
 		key := genKey(s.Count()) // generate the short URL
 		if ok := s.Set(key, url); ok {
+			// 先做持久化, 再返回key
+			if err := s.save(key, url); err != nil {
+				log.Println("Error saving to URLStore: ", err)
+			}
 			return key
 		}
 	}
 	// shouldn't get here
-	return ""
+	panic("shouldn't get here")
 }
 
 // save 将给定的 key 和 url 作为一个 gob 编码的 record 写入到磁盘
 func (s *URLStore) save(key, url string) error {
 	e := gob.NewEncoder(s.file) // e为编码器
 	return e.Encode(record{key, url})
+}
+
+// load 在程序启动后, 需要将磁盘上的数据读到URLStore中
+func (s *URLStore) load() error {
+	if _, err := s.file.Seek(0, 0); err != nil {
+		return err
+	}
+	d := gob.NewDecoder(s.file) // 解码器
+	var err error               // 声明error类型变量
+	for err == nil {
+		var r record
+		if err = d.Decode(&r); err == nil {
+			s.Set(r.Key, r.URL)
+		}
+	}
+	if err == io.EOF {
+		return nil
+	}
+	return err
 }
 
 /*
